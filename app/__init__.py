0ec56a02ce8d2d7973058e810d5cf53324f6563f
Rayyan Ahmed <syedrayyaann00@gmail.com>
2024-12-24 15:59:09 +0530

    Refactor Flask app initialization and update Vercel configuration formatting

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,9 @@ from flask import Flask
 from app.routes.main import main  # Import the main blueprint
 
 def create_app():
+    
     app = Flask(__name__, static_folder='static', template_folder='templates')
-    app.register_blueprint(main)  # Register the blueprint here
+    from .routes.main import main_bp
+    app.register_blueprint(main_bp)
+    
     return app
